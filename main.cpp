092dea68655372969bd99d76ac88c7c1dc3200dc
Kseniya-Gnezdilova <115429521+Kseniya-Gnezdilova@users.noreply.github.com>
2023-05-18 21:02:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+#include <QFrame>
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
      MainWindow scene;
      QGraphicsView view(&scene);
-     scene.setSceneRect(0,0,1024,1024);
-     view.show();
-     a.exec();
+     scene.setSceneRect(80,100 , view.width(), view.height());
+     view.setRenderHint(QPainter::Antialiasing);
+     view.showFullScreen();
      return QApplication::exec();
 }
