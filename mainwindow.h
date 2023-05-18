a66c4b10f1dcd587255df8089d21f1467caf7402
Kseniya-Gnezdilova <115429521+Kseniya-Gnezdilova@users.noreply.github.com>
2023-05-18 21:02:51 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -10,5 +10,10 @@ public:
 protected:
    void keyPressEvent(QKeyEvent *event) override;
    void keyReleaseEvent(QKeyEvent *event) override;
+   void uploadAttack();
+   void attack1();
+   void attack2();
+   void attack3();
+   void attack4();
    Model *model;
 };
