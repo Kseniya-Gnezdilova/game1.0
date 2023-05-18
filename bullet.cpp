38f654fcf3ceaaa654b1e55e0b6f8ed2410f07c4
Kseniya-Gnezdilova <115429521+Kseniya-Gnezdilova@users.noreply.github.com>
2023-05-18 20:59:17 +0300

    Update bullet.cpp

--- bullet.cpp
+++ bullet.cpp
@@ -2,7 +2,7 @@
 
 bullet::bullet(){
    bulletSpeed = 8;
-   bulletDamage = 1;
+   bulletDamage = 2;
 }
 
 void bullet::setSpeed(int speed){
@@ -12,11 +12,24 @@ int bullet::getSpeed() const{
    return bulletSpeed;
 }
 void bullet::setDirection(const QPointF& direction_){
-   direction = direction_;
+   direction = direction_ - getPosition();
 }
 QPointF bullet::getDirection() const{
    return direction;
 }
 int bullet::getDamage() const{
    return bulletDamage;
+}
+void bullet::move(){
+   move(getDirection());
+}
+void bullet::move(const QPointF& direction){
+   double length = sqrt(pow(direction.x(),2) + pow(direction.y(),2));
+   if (length == 0)
+       return;
+   setPosition({pos_.x() + getSpeed() * direction.x() / length, pos_.y() + getSpeed() * direction.y() / length});
+}
+
+void bullet::setDamage(const int dmg){
+   bulletDamage = dmg;
 }
