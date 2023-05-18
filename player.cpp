8c5d2c0c5586e6c08e9c4eb30d415eb3d38f05be
Kseniya-Gnezdilova <115429521+Kseniya-Gnezdilova@users.noreply.github.com>
2023-05-18 21:04:31 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,40 +1,29 @@
-#include "player.h"
+#pragma once
+#include "gameobject.h"
+#include <vector>
+#include <QWidget>
 #include <QPainter>
-player::player(){
-   playerHealth = 1000;
-   playerSpeed = 5;
-   //setHitBox({0,0},{30,30});
-   setPosition({0,0});
-   setDirection({0, 0});
-}
-void player::setHealth(const int health){
-   playerHealth = health;
-}
-int player::getHealth() const{
-   return playerHealth;
-}
-void player::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) {
-   painter->setPen({Qt::black, 2});
-   painter->drawRect(boundingRect());
-}
+#include <QStyleOptionGraphicsItem>
+#include <QPoint>
+#include <QGraphicsView>
 
-QRectF player::boundingRect() const {
-   return QRectF(-10, -10, 20, 20);
-}
+class player : public GameObject {
+protected:
+   int playerHealth;
+   int playerDamage = 1;
+   int playerSpeed;
+   std::vector<int> inventory;
+   QPointF playerDirection;
+ public:
+   void setHealth(const int health);
+   int getHealth() const;
+   void setDirection(const QPointF& direction);
+   const QPointF& getDirection() const;
+   void move(const QPointF& direction);
+   player();
+   const int& getSpeed() const;
+   void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+   QRectF boundingRect() const override;
+};
 
-const int& player::getSpeed() const{
-   return playerSpeed;
-}
 
-void player::setDirection(const QPointF& direction){
-   playerDirection = direction;
-}
-const QPointF& player:: getDirection() const{
-   return playerDirection;
-}
-void player::move(const QPointF& direction){
-   double length = sqrt(pow(direction.x(),2) + pow(direction.y(),2));
-   if (length == 0)
-      return;
-   setPosition({std::max(std::min(pos_.x() + playerSpeed * direction.x() / length, 690.),110.), std::max(std::min(pos_.y() + playerSpeed * direction.y() / length, 690.),110.)});
-}
